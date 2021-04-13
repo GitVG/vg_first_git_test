0419f89dd15d2ddde206393a4ca6327ed82beb3c
GitVG <lheart@ya.ru>
2021-04-13 10:08:26 +0300

    added textboxtestscor

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -16,41 +16,33 @@ public class TextBoxTests {
     }
 
     @Test
-    void dataAppear()
-    {
+    void dataAppear() {
         String dateOfBirthDay = "6",
                 dateOfBirthMonth = "July",
                 firstName = "Billy",
                 lastName = "Willy",
                 dateOfBirthYear = "1988";
-
         open("https://www.demoqa.com/automation-practice-form");
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue("WB@mail.ru");
-        $(By.xpath("//label[contains(text(),'Male')]")).click();
-        $(By.xpath("//div[@class='col-md-9 col-sm-12']/input[@id='userNumber']")).setValue("1234567890");
-      /*$(By.xpath("//div[@class='react-datepicker__input-container']/input")).click();
-        $(By.xpath("//div[@class ='react-datepicker__month-dropdown-container react-datepicker__month-dropdown-container--select']/select[@class='react-datepicker__month-select']/option[6]")).click();
-        $(By.xpath("//div[@class ='react-datepicker__year-dropdown-container react-datepicker__year-dropdown-container--select']/select[@class='react-datepicker__year-select']/option[123]")).click();
-        $(By.xpath("//div[@class ='react-datepicker__day react-datepicker__day--015']")).click();
-      */
+        $x("//label[contains(text(),'Male')]").click();
+        $x("//input[@id='userNumber']").setValue("1234567890");
         $x("//input[@id='dateOfBirthInput']").click();
         if (dateOfBirthDay.length() == 1) dateOfBirthDay = "0" + dateOfBirthDay;
         $x("//select[@class='react-datepicker__year-select']").selectOption(dateOfBirthYear);
         $x("//select[@class='react-datepicker__month-select']").selectOption(dateOfBirthMonth);
-        $x("//div[contains(@class,'react-datepicker__day--0"+dateOfBirthDay+"')]").click();
+        $x("//div[contains(@class,'react-datepicker__day--0" + dateOfBirthDay + "')]").click();
         $x("//input[@id = 'subjectsInput']").setValue("Eng");
         $x("//*[text()='English']").click();
-        $(By.xpath("//div[@class='custom-control custom-checkbox custom-control-inline']/label[contains(text(), 'Music')]")).click();
-      //  $(By.xpath("//div[@class='form-file']/input")).sendKeys("C:\\Users\\Владимир Горбунов\\OneDrive - ООО АЙФЭЛЛ\\Рисунки\\Saved Pictures\\1.jpg");
+        $x("//div[contains(@class, 'custom-control-inline')]/label[text() = 'Music']").click();
         $x("//input[@id='uploadPicture']").uploadFromClasspath("img/1.jpg");
-        $(By.xpath("//div[@class='col-md-9 col-sm-12']/textarea")).setValue("Lorem Ipsum");
+        $x("//div[@class='col-md-9 col-sm-12']/textarea").setValue("Lorem Ipsum");
         $x("//*[@id='react-select-3-input']").setValue("NC");
         $x("//*[text()='NCR']").click();
         $x("//*[@id='react-select-4-input']").setValue("De");
         $x("//*[text()='Delhi']").click();
-        $("#submit").click(); // Проверка формы
+        $x("//*[@id='submit']").click(); // Проверка формы
         $x("//td[(text()='Student Name')]").parent().shouldHave(text(firstName + " " + lastName));
         $x("//td[(text()='Student Email')]").parent().shouldHave(text("WB@mail.ru"));
         $x("//td[(text()='Gender')]").parent().shouldHave(text("Male"));
@@ -61,6 +53,6 @@ public class TextBoxTests {
         $x("//td[(text()='Picture')]").parent().shouldHave(text("1.jpg"));
         $x("//td[(text()='Address')]").parent().shouldHave(text("Lorem Ipsum"));
         $x("//td[(text()='State and City')]").parent().shouldHave(text("NCR" + " " + "Delhi"));
-        sleep(5000);
+
     }
 }
